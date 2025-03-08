ecde2e078e4b9634413f42c9ccbc4f69409d4a37
SaRa-Harahsheh <saraharahsheh0@gmail.com>
2025-03-09 01:08:24 +0300

    TS Task

--- Task1/Task1.js
+++ Task1/Task1.js
@@ -1,24 +1,23 @@
 //task 6-3-2025
 //task 1: Identify if a Number is Even or Odd
-var num = 3;
+var num = 1;
 if (num % 2 == 0) {
-    console.log("even number");
+    console.log("The ".concat(num, " is even"));
 }
 else {
-    console.log("odd number");
+    console.log("The ".concat(num, " is odd"));
 }
 //task 2: Filter Expensive Products from an Array 
-var products = [16, 30, 22, 30, 43];
-var threshold = 20;
-products.forEach(function (element) {
-    if (threshold < element) {
-        console.log(element);
-    }
-});
+var PriceOfProduct = [100, 200, 150, 300];
+var threshold = 170;
+for (var i = 0; i < PriceOfProduct.length; i++) {
+    if (PriceOfProduct[i] > threshold)
+        console.log(PriceOfProduct[i]);
+}
 //task 3: Sum All Elements in an Array
-var count = 0;
-var numbers = [30, 7, 23, 19, 34];
-numbers.forEach(function (element) {
-    count += element;
-});
-console.log(count);
+var ArrayOfNum = [1, 2, 3, 4, 5];
+var sum = 0;
+for (var i = 0; i < ArrayOfNum.length; i++) {
+    sum = sum + ArrayOfNum[i];
+}
+console.log("The sum is " + sum);
--- Task1/Task1.ts
+++ Task1/Task1.ts
@@ -1,25 +1,40 @@
 //task 6-3-2025
+
+
 //task 1: Identify if a Number is Even or Odd
-let num : number = 3;
-if (num%2==0) {
-    console.log("even number");
-}else{
-    console.log("odd number");
+
+let num:number = 1;
+
+if (num %2==0){
+    console.log (`The ${num} is even`)
+}
+else 
+{
+    console.log(`The ${num} is odd`)
 }
 
 //task 2: Filter Expensive Products from an Array 
-let products : number[]=[16, 30, 22, 30, 43];
-let threshold : number = 20;
-products.forEach(element => {
-    if (threshold<element) {
-        console.log(element)
-    }
-});
+
+let PriceOfProduct : number[] = [100,200,150,300]
+let threshold :number = 170;
+
+for(let i=0;i<PriceOfProduct.length;i++)
+{
+if (PriceOfProduct[i]>threshold )
+    console.log(PriceOfProduct[i]);
+}
+
+
 
 //task 3: Sum All Elements in an Array
-let count:number = 0;
-let numbers:number[] = [30,7,23,19,34]
-numbers.forEach(element => {
-    count += element;
-});
-console.log(count);
+
+
+let ArrayOfNum :number[]= [1,2,3,4,5]
+let sum = 0;
+
+ for(let i=0;i<ArrayOfNum.length;i++)
+    {
+sum =sum+ArrayOfNum[i]
+
+ }
+ console.log("The sum is " + sum);
